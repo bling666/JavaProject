e3615a9df9387d98b916a3ae07008d894f645d61
lizhichao <lizhichao@pku.edu.cn>
2020-05-04 18:39:02 +0800

    feat:add global return data structure. please see api.md for the details.

--- backend/src/main/java/com/example/backend/service/vocabularyController.java
+++ backend/src/main/java/com/example/backend/service/vocabularyController.java
@@ -1,6 +1,8 @@
 package com.example.backend.service;
 
 import com.example.backend.Entity.vocabulary;
+import com.example.backend.utility.baseResult;
+import com.example.backend.utility.resultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +19,23 @@ public class vocabularyController {
 
     @RequestMapping("getoneword")
     @ResponseBody
-    public List<vocabulary> get_one_word(String keyword,Integer top)
+    public baseResult<List<vocabulary>> get_one_word(String keyword, Integer top)
     {
-        System.out.println(keyword);
-        System.out.println(top);
-        if(top==null)
-        {
-            top = 1;
+        try {
+            System.out.println(keyword);
+            System.out.println(top);
+            if (top == null) {
+                top = 1;
+            }
+            List<vocabulary> re = vocabularyrepo.findAllByWordLike(keyword, top);
+            if(re.isEmpty())
+                return resultUtil.error(404,"no result");
+            return resultUtil.success(re);
         }
-        return vocabularyrepo.findAllByWordLike(keyword,top);
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return resultUtil.error(555,"something wrong but i don't know,please call me");
+        }
     }
 }
---
+++ backend/src/main/java/com/example/backend/utility/resultUtil.java
@@ -0,0 +1,24 @@
+package com.example.backend.utility;
+
+public class resultUtil {
+    public static <T> baseResult<T> success(T data) {
+        return commonResult(1, 200, "success", data);
+    }
+
+    public static <T> baseResult<T> error(String errorMsg) {
+        return error(200, errorMsg);
+    }
+
+    public static <T> baseResult<T> error(Integer code, String errorMsg) {
+        return commonResult(0, code, errorMsg, null);
+    }
+
+    private static <T> baseResult<T> commonResult(Integer status, Integer code, String errMsg, T data) {
+        baseResult<T> result = new baseResult<>();
+        result.setStatus(status);
+        result.setCode(code);
+        result.setMsg(errMsg);
+        result.setData(data);
+        return result;
+    }
+}
