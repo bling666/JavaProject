b1e156e0d9be21e0f80a4e03809753a251258642
jiang13021 <45600652+jiang13021@users.noreply.github.com>
2020-05-13 05:05:29 +0800

    Update Processor.java
    增加了计算单词重要性部分的内容

--- Algorithm/Processor.java
+++ Algorithm/Processor.java
@@ -1,26 +1,38 @@
+package com.example.backend.core;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.Map.Entry;
+
+import com.example.backend.Entity.UserVocabulary;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class Processor{
-    public static String[] ReadAndToken(String filepath) throws IOException{
+	private static int forgettingCurve[] = { //一共8个背诵阶段
+			0,
+			5,			// 5分钟 
+			30,			// 30分钟 
+			12*60,		// 12小时 
+			1*24*60,	// 1天
+			2*24*60,	// 2天 
+			4*24*60,	// 4天 
+			7*24*60,	// 7天 
+			15*24*60	// 15天 
+	};
+    public static String[] ReadAndToken(String raw) throws IOException{
         //读取停用词表
-        List<String> stopwords =  Files.readAllLines(Paths.get("D:\\Sophomore2\\java_project\\stopwords.txt"));
+        List<String> stopwords =  Files.readAllLines(Paths.get("D:/stopwords.txt"));
         //读取文件
-        List<String> lines = Files.readAllLines(Paths.get(filepath));
-        Iterator<String> it = lines.iterator(); 
         List<String> tokenlist = new ArrayList<String>();
         TreeMap<String, Integer> countMap = new TreeMap<>();//用于去重和计数
-        while(it.hasNext()){
-            String temp = it.next();
+            String temp = raw;
             String[] templist = temp.split("[^A-Za-z]");//这里只保留了字母
             for(String word : templist){
                 if(word != null && word.length() > 0 && !stopwords.contains(word.toLowerCase())){//除去停用词和空String
@@ -33,10 +45,9 @@ public class Processor{
                     }
                 }
             }
-        }
         List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(countMap.entrySet());
-		
-		Collections.sort(list,new Comparator<Map.Entry<String,Integer>>() {
+
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>() {
             //降序排序
             public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
                 return o2.getValue().compareTo(o1.getValue());
@@ -48,4 +59,24 @@ public class Processor{
         String[] token = (String[])tokenlist.toArray(new String[tokenlist.size()]);
         return token;
     }
+	
+	// 根据记忆曲线，计算单词的deltstate
+	public static int DeltState(UserVocabulary record) {
+		long currentTime = System.currentTimeMillis();
+		int timeDiff = (int)((currentTime - record.getLast_update().getTime()) / (1000 * 60)); //单位min
+		
+		if (timeDiff > forgettingCurve[record.getStage()]) {//如果超过了已知阶段的对应时长，则安排背诵
+			for(int i = 0;i < 9;i++) {
+				if(forgettingCurve[i] >= timeDiff)
+					return i - record.getStage();
+			}
+		}
+		return 0;//else
+	}
+	//计算重要度
+	public static double cal_Importance(UserVocabulary record) {
+		double alpha = 1.0;
+		double beta = 1.0;
+		return alpha * record.getFrequency() + beta * DeltState(record);
+	}
 }
